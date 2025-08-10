69c5d7635534c153848bd6b1c0ae3ff66391dfa8
Jona0002 <jona0002@student.monash.edu>
2025-08-11 03:51:45 +0800

    Add new constants and types for user profile attributes

--- constants/data.ts
+++ constants/data.ts
@@ -1,18 +1,24 @@
-export const states = [
-    "Johor",
-    "Kedah",
-    "Kelantan",
-    "Kuala Lumpur",
-    "Labuan",
-    "Melaka",
-    "Negeri Sembilan",
-    "Pahang",
-    "Penang",
-    "Perak",
-    "Perlis",
-    "Putrajaya",
-    "Sabah",
-    "Sarawak",
-    "Selangor",
-    "Terengganu"
-]
+export const STATES = [
+  'Johor',
+  'Kedah',
+  'Kelantan',
+  'Kuala Lumpur',
+  'Labuan',
+  'Melaka',
+  'Negeri Sembilan',
+  'Pahang',
+  'Penang',
+  'Perak',
+  'Perlis',
+  'Putrajaya',
+  'Sabah',
+  'Sarawak',
+  'Selangor',
+  'Terengganu',
+] as const;
+
+export const AGE_RANGE = ['<18', '18-29', '30-39', '40-49', '50-69', '70+'] as const;
+export type State = typeof STATES[number];
+export type AgeRange = typeof AGE_RANGE[number];
+export type UserType = "deaf_user" | "interpreter";
+export type Gender = "Male" | "Female"
