c5c136118c824954a6c40f2b831a01446234097d
Jona0002 <jona0002@student.monash.edu>
2025-08-27 06:07:31 +0800

    Removes outdated search method from helper file

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,5 +1,4 @@
 import { AgeRange, AGE_RANGE, Spec, Language, State } from "@/constants/data";
-import { supabase } from "./supabase";
 
 export const getAgeRangeFromDOB = (dateOfBirth: string): AgeRange => {
   const age = Math.floor((Date.now() - new Date(dateOfBirth).getTime()) / (1000 * 60 * 60 * 24 * 365));
@@ -17,40 +16,6 @@ export const getMinMaxDOB = (ageStart: number, ageEnd: number) => {
   const maxDOB = new Date();
   maxDOB.setFullYear(maxDOB.getFullYear() - ageStart);
   return { minDOB, maxDOB };
-};
-
-export const searchInterpreters = async (
-  spec: Spec,
-  language: string,
-  state: string,
-  ageStart: number,
-  ageEnd: number,
-  gender: string | null = null
-) => {
-  const { minDOB, maxDOB } = getMinMaxDOB(ageStart, ageEnd);
-
-  let query = supabase
-    .from("interpreter_profile")
-    .select(
-      `
-      *,
-      profile (*)
-    `
-    )
-    .eq(spec, true)
-    .eq(language, true)
-    .eq("profile.location", state)
-    .gt("profile.date_of_birth", minDOB.toISOString())
-    .lt("profile.date_of_birth", maxDOB.toISOString())
-    .not("profile", "is", null)
-    .limit(5);
-
-  if (gender) {
-    query = query.order("profile.gender", { ascending: gender === "Female" });
-  }
-
-  const { data } = await query;
-  return data;
 };
 
 export const getMeetLink = async (providerToken: string) => {
